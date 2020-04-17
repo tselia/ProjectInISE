b3a138ee7027090685a2c8b6ee71d40d118e0ff5
polinafr <57112074+polinafr@users.noreply.github.com>
2020-04-17 16:48:39 +0300

    17/04/20 Plane findIntersections() impplemented

--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -2,18 +2,20 @@ package geometries;
 
 import primitives.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Class plane is a basic representation of plane
  * Authors - Polina Frolov Korogodsky and Tselia Tebol
  */
-public class Plane implements Intersectable{
+public class Plane implements Intersectable {
     Point3D point;
     Vector normal;
 
     /**
      * Constructor with point and vector
+     *
      * @param point
      * @param normal
      */
@@ -24,6 +26,7 @@ public class Plane implements Intersectable{
 
     /**
      * finds a normal and defines a plane by normal and point
+     *
      * @param A
      * @param B
      * @param C
@@ -34,18 +37,18 @@ public class Plane implements Intersectable{
         Vector BC = new Vector(B.subtract(C));
         try {
             this.normal = AB.crossProduct(BC).normalized();// don't have any idea whether it works
-        }
-        catch ( ArithmeticException ex){
+        } catch (ArithmeticException ex) {
             throw ex;
         }
-        if( A.equals(Point3D.zero)||B.equals(Point3D.zero)||C.equals(Point3D.zero))
+        if (A.equals(Point3D.zero) || B.equals(Point3D.zero) || C.equals(Point3D.zero))
             this.point = Point3D.zero;
-        else  this.point = A;
+        else this.point = A;
 
     }
 
     /**
      * returns normal
+     *
      * @return vector
      */
     public Vector getNormal() {
@@ -56,6 +59,7 @@ public class Plane implements Intersectable{
 
     /**
      * returns a point that by it a plane is defined
+     *
      * @return point
      */
     public Point3D getPoint() {
@@ -69,9 +73,40 @@ public class Plane implements Intersectable{
                 ", normal=" + normal +
                 '}';
     }
+
     @Override
     public List<Point3D> findIntersections(Ray ray) {
-        return null;
-    }
+        if (ray.getStart().equals(point))
+            return null;
+        if(ray.getDirection().dotProduct(normal)==0)//the ray is parallel to the plane
+            return null;
+        double n = normal.dotProduct(ray.getDirection());
+        if (!Util.isZero(n)) {
+            double coefficient = normal.dotProduct(point.subtract(ray.getStart())) / n;
+            if (coefficient <= 0)
+                return null;
+            //if()
+            else {
+                List<Point3D> intersectionPoints = new ArrayList<Point3D>();
+                //Point3D intersectionPoint = new Point3D(ray.getStart().add(ray.getDirection().scale(coefficient)));
+                intersectionPoints.add(new Point3D(ray.getStart().add(ray.getDirection().scale(coefficient))));
+                return intersectionPoints;
+            }
+        } else {
+            double sum = point.getX().get() * normal.getEnd().getX().get() + //so we can get to the plane representation as ax+by+cz=d
+                    point.getY().get() * normal.getEnd().getY().get() +
+                    point.getZ().get() * normal.getEnd().getZ().get();
+            double coefficient = (sum - ray.getStart().getX().get() - ray.getStart().getY().get() - ray.getStart().getZ().get()) /
+                    (ray.getDirection().getEnd().getX().get() + ray.getDirection().getEnd().getY().get() + ray.getDirection().getEnd().getZ().get());
+            if (coefficient <= 0)
+                return null;
+            else {
+                List<Point3D> intersectionPoints = new ArrayList<Point3D>();
+                //Point3D intersectionPoint = new Point3D(ray.getStart().add(ray.getDirection().scale(coefficient)));
+                intersectionPoints.add(new Point3D(ray.getStart().add(ray.getDirection().scale(coefficient))));
+                return intersectionPoints;
+            }
+        }
 
+    }
 }
